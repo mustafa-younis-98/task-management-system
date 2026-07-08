8baf3802a815df6650bac1706013271ba8ddf13e
Mustafa Younis <don.mu@bk.ru>
2026-07-09 02:50:05 +0300

    Add loading states to dashboard, task details, and edit task

--- src/app/features/tasks/pages/task-details/task-details.ts
+++ src/app/features/tasks/pages/task-details/task-details.ts
@@ -16,18 +16,25 @@ export class TaskDetails {
 
   task?: Task;
 
+  isLoading = false;
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
 
     if (!id) return;
 
+    this.isLoading = true;
+
     this.taskService.getTaskById(id).subscribe({
       next: (task) => {
         this.task = task;
 
+        this.isLoading = false;
+
         console.log(task);
       },
       error: (err) => {
+        this.isLoading = false;
         console.error(err);
       },
     });
