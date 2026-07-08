f58363d836114380ef35333e0c988c2aebeed012
Mustafa Younis <don.mu@bk.ru>
2026-07-08 22:20:48 +0300

    Add sidebar navigation

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
