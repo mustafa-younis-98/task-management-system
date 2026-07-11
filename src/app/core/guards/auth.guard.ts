c6050175b47975e981377dfb5c8644ec2d4910a5
Mustafa Younis <don.mu@bk.ru>
2026-07-11 19:43:50 +0300

    Show toast message in auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,14 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { ToastService } from '../services/toast.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const toastService = inject(ToastService);
 
   const token = localStorage.getItem('token');
 
   if (token) {
     return true;
   }
+
+  toastService.show('Please log in to continue.', 'error');
+
   router.navigate(['/login']);
 
   return false;
